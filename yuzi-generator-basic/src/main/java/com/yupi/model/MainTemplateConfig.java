c716dc9eba0f84f14974aa2781204067cd9291c0
wudada <2567199685@qq.com>
2024-12-17 08:29:15 +0800

    引入模板依赖

---
+++ yuzi-generator-basic/src/main/java/com/yupi/model/MainTemplateConfig.java
@@ -0,0 +1,13 @@
+package com.yupi.model;
+
+import lombok.Data;
+
+/**
+ * 静态模板配置
+ */
+@Data
+public class MainTemplateConfig {
+    private String author;
+    private String outputText;
+    private boolean loop;
+}
