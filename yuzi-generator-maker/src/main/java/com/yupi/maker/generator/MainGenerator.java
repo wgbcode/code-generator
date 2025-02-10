7e7471942e6eb9724c029d0d56b42263eec304ee
wudada <2567199685@qq.com>
2025-02-10 08:48:13 +0800

    封装脚本

--- yuzi-generator-maker/src/main/java/com/yupi/maker/generator/MainGenerator.java
+++ yuzi-generator-maker/src/main/java/com/yupi/maker/generator/MainGenerator.java
@@ -88,8 +88,15 @@ public class MainGenerator {
 
         // 构建 jar 包
         JarGenerator.doGenerate(outputPath);
+
+        // 封装脚本
+        String shellOutputFilePath = outputPath + File.separator + "generator";
+        String jarName = String.format("%s-%s-jar-with-dependencies.jar", meta.getName(), meta.getVersion());
+        String jarPath = "target/" + jarName;
+        ScriptGenerator.doGenerate(shellOutputFilePath, jarPath);
     }
 }
+
 
 
 
